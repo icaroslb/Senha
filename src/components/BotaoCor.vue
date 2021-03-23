1880ff801a0c54ce7abdd0ac5c2876b557445329
Nathan Serra <ant.nathan15@gmail.com>
2021-03-23 18:30:59 -0300

    fix: refatorado nome de componentes
    adicionado lógica para adicionar novos campos
    adicionado atualizações de valores para serem analisados no jogosenha
    refatorado algumas funções para enxugar o código

---
+++ src/components/BotaoCor.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="botao">
+    <circulo
+      class="acionador"
+      :class="cor"
+      :cor_pai="cor"
+      v-on:click="troca_cor()"
+    ></circulo>
+  </div>
+</template>
+
+<script>
+import circulo from "./Circulo.vue";
+
+export default {
+  name: "botao_cor",
+
+  components: { circulo },
+
+  props: ["cor"],
+
+  data: function () {
+    return {
+      cor_pai: this.cor,
+    };
+  },
+
+  methods: {
+    troca_cor: function () {
+      this.$emit("click");
+    },
+  },
+};
+</script>
+
+<style></style>
